28a68fe70f0380267efc0feb0b4b17fd21491185
Jespson <jespstpierre@yahoo.com>
2023-04-14 13:36:21 -0400

    login page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { AppContextProvider } from "./context/AppContext";
 import Home from "./pages/Home";
 import Chat from "./pages/ChatPage";
 import Register from "./pages/Register";
@@ -10,13 +11,16 @@ init();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/tutor" element={<Chat />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <AppContextProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/tutor" element={<Chat />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </AppContextProvider>
   );
 }
 
