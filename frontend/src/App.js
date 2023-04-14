6781dba3e719dc90c14c0bbc6f15fd1fec148d37
Jespson <jespstpierre@yahoo.com>
2023-04-14 13:36:21 -0400

    login page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AppContextProvider } from './context/AppContext';
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PrivateRoute from './components/PrivateRoute';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <AppContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <PrivateRoute exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </AppContextProvider>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,66 @@
+import { useState, useContext } from 'react';
+import swal from 'sweetalert';
+import { AppContext } from '../context/AppContext';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function Login({ history }){
+    const [formData, setFormData] = useState(null);
+    const { currentUser, setCurrentUser } = useContext(AppContext);
+    const [error, setError] = useState(null);
+    
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+        const response = await axios.post('/api/users/login', formData);
+        setCurrentUser(response.data);
+        sessionStorage.setItem('user', response.data);
+        setError(null);
+        history.push('/');
+        } catch (error) {
+        swal(`Oops!`, 'Something went wrong.');
+        setError(error.response.data.message);
+        }
+  };
+
+    return(
+        <div id="login">
+        <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+                <label id="email">Email </label>
+                <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                ></input>
+                <div id="passlabel">
+                <label className="passwordLabel">Password </label>
+                <input
+                    className="passwordInput"
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                ></input>
+                </div>
+                {error && <p>Error: {error}</p>}
+                <div>
+                <button className="loginBtn" type="submit">
+                    <img
+                    id="loginImg"
+                    src="https://cdn-icons-png.flaticon.com/512/854/854184.png"></img>
+                </button>
+                </div>
+            </form>
+            <div>
+                <p>New Here</p>
+                <Link to="/Register">Click here to make an account</Link>
+            </div>
+    </div>
+    )
+}
+
+export default Login
