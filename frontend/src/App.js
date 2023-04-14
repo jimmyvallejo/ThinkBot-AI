3475569ec1c0f50142ebbb652282c9a34780e988
Jimmy <jrv1295@gmail.com>
2023-04-14 19:06:02 -0400

    Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext";
 import Home from "./pages/Home";
 import Chat from "./pages/ChatPage";
-<<<<<<< HEAD
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
