67288bb79439fe4be553fde905015aa33ba8e78f
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-04-14 23:19:29 -0400

    setup login and logout auth with backend

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,41 +1,36 @@
 import React, { createContext, useState, useEffect } from "react";
-// import axios from 'axios';
-// import swal from 'sweetalert';
-// import { useHistory } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import axios from "axios";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  //   const [currentUser, setCurrentUser] = useState(null);
+  const [user, setUser] = useState(null);
   //   const [loading, setLoading] = useState(false);
   //   const history = useHistory();
 
-  //   let user = sessionStorage.getItem('user');
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), async (authUser) => {
+      if (authUser?.uid && !user) {
+        const data = await axios.get(
+          `http://127.0.0.1:5001/miami-hackathon-ai/us-central1/api/users/${authUser.uid}`
+        );
 
-  //   useEffect(() => {
-  //     user = sessionStorage.getItem('user');
-  //     if (user && !currentUser) {
-  //       axios
-  //         .get('/api/users/me', { withCredentials: true })
-  //         .then(({ data }) => {
-  //           setCurrentUser(data);
-  //         })
-  //         .catch((error) => {
-  //           swal('Oops!', error.toString());
-  //         });
-  //     }
-  //   }, [currentUser, user]);
+        setUser(data.data);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return subscriber;
+  }, []);
 
   return (
-    // <AuthContext.Provider
-    //   value={{
-    //     currentUser,
-    //     setCurrentUser,
-    //     loading,
-    //     setLoading,
-    //   }}
-    // >
-    //   {children}
-    // </AuthContext.Provider>
-    <></>
+    <AuthContext.Provider
+      value={{
+        user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,20 +2,6 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 function Home() {
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await axios.get(
-          "http://127.0.0.1:5001/miami-hackathon-ai/us-central1/api/users/my-test-id"
-        );
-
-        console.log(res);
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  });
-
   return <h1>Home</h1>;
 }
 
