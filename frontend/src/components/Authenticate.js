9343d12d6592f2f19332530cfdce4bf0e7078fa5
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-04-15 18:02:36 -0400

    wip

--- frontend/src/components/Authenticate.js
+++ frontend/src/components/Authenticate.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 const Authenticate = ({ component: Component, ...rest }) => {
-  //   const navigate = useNavigate();
-  //   const user = sessionStorage.getItem("user");
-  const user = {};
+  const { user } = useContext(AuthContext);
 
   return user ? <Component /> : <Navigate to="/login" replace />;
 };
